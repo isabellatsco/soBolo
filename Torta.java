64fb155490c295a75fce34e5e8945d3ae5e19df9
Isabella Costa <166655665+isabellatsco@users.noreply.github.com>
2026-01-18 20:35:58 -0300

    + classe Torta

---
+++ Torta.java
@@ -0,0 +1,41 @@
+package soBolos;
+
+public class Torta extends Bolo {
+	private boolean recheio = true;
+	private boolean cobertura = true;
+	
+	public Torta(int codigo, double custo, IFormato formato) {
+		super(codigo, custo, formato);
+	}
+	
+	public Torta(int codigo, double custo, IFormato formato, boolean recheio, boolean cobertura) {
+		super(codigo, custo, formato);
+		this.setRecheio(recheio);
+		this.setCobertura(cobertura);
+	}
+
+	public boolean isRecheio() { return recheio;}
+	public void setRecheio(boolean recheio) { this.recheio = recheio;}
+
+	public boolean isCobertura() { return cobertura; }
+	public void setCobertura(boolean cobertura) { this.cobertura = cobertura; }
+
+	@Override
+	public double preco() {
+		double precoBase = this.getCusto() * this.getFormato().area();
+		double precoRecheio = this.isRecheio() ? 20 : 0;
+		double precoCobertura = this.isCobertura() ? 12.50 : 0;
+		return precoBase + precoRecheio + precoCobertura;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " Bolo Simples\n" +
+        	"⤷ Preço: " + this.preco() +
+        	" | Formato: " + this.getFormato().getClass().getSimpleName() +
+			(this.isRecheio() ? "Sem" : "Com") + " Recheio" +
+			(this.isCobertura() ? "Sem" : "Com") + " Cobertura";
+        
+    }
+	
+}
